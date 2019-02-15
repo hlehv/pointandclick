02e57918cd0bbd590d7c56a549ec35a6d26d681d
Benjamin Most <benjaminmost@gmail.com>
2019-02-15 15:52:31 -0500

    Working on setting up background

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Sidebar />
+		<background />
       </div>
     );
   }
---
+++ client/src/components/background/Background.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Background extends React.Component {
+  render() {
+    return (
+      <div class="background">
+        <a href ="#">Mom</a>
+        <a href = "#">Dad</a>
+        <a href = "#">Grandpappy</a>
+        <a href = "#">Aunty Sue</a>
+      </div>
+    );
+  }
+}
+export default Background;
