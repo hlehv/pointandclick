2ebdd71a31dd6bf5677bbc4c7322f1b58b1c2dd8
hlehv <hlehv@aol.com>
2019-04-19 19:08:24 -0400

    Log in works

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,35 @@ import React from "react";
 import Desktop from "./Screens/Desktop/Desktop";
 import LogIn from "./Screens/LogIn/LogIn";
 
+
+
+
 class App extends React.Component {
   state = {
     loggedIn: true
   };
+  
   render() {
     if(this.state.loggedIn){
       return <Desktop />;
     } else {
-      return <LogIn />;
+      return this.renderLogIn();
     }
   }
+
+  renderLogIn() {
+    return (
+      <div className="App">
+        <LogIn onValidPassword={this.setIsValid} />
+      </div>
+    );
+  }
+
+  setIsValid = () => {
+    this.setState({
+      loggedIn: true
+    })
+  }
+
 }
 export default App;
